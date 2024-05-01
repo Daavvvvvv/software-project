919d0c24bf30baad36a589145bbf09979a5382e6
Daavvvvvv <davidvelez006@gmail.com>
2024-05-01 09:47:14 -0500

    changes

--- RandomQuotes.py
+++ RandomQuotes.py
@@ -5,13 +5,11 @@ import random
 app = Flask(__name__)
 
 phrases = [
-    "Get ready to be inspired…", 
-    "See rejection as redirection.",
-    "There is beauty in simplicity.",
-    "You can’t be late until you show up.",
-    "Maybe life is testing you. Don’t give up.",
-    "Impossible is just an opinion.",
-    "Alone or not you gonna walk forward.",
+    "Sevval Ates is the most beautiful girl everr"
+    "The best way to live in the future is living with you"
+    "Sevval mean the world to me"
+    "Ates mean the life to me"
+    "I LOVE YOUUUUUUUUUUUU SEVVAAAAAAAAAAAAAAAAAAAL"
 ]
 
 
